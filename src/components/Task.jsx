caba27b4c2282959e3d667dd9b2cb78d1c160a21
Sunil Park <sunilpark1129@gmail.com>
2023-05-11 21:51:22 -0400

    test6

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -40,9 +40,8 @@ export default function Task({ title, state, currentIdx }) {
   function dragStart(e) {
     // setAction(false);
     setDragged(true);
-    if (windows.navigator && windows.navigator.vibrate) {
-      navigator.vibrate(100);
-    }
+
+    navigator.vibrate(100);
   }
   function drag(e) {
     if (hasDragged) {
